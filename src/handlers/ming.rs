5850d03eaaff6c6b8f8efe51c75fe5490e3d18b4
Kal.l <kalvin807@gmail.com>
2022-11-19 16:24:46 +0900

    capture all regex match instead of first

--- src/handlers/ming.rs
+++ src/handlers/ming.rs
@@ -10,10 +10,19 @@ lazy_static! {
         Regex::new(r"(http(s)?://)?(www.)?([a-zA-Z0-9])+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(/[^\s]*)?").unwrap();
 }
 
-fn has_url(url: String) -> Option<String> {
-    URL_RE
-        .find(&url)
-        .map(|matches| matches.as_str().to_string())
+fn has_url(url: String) -> Option<Vec<String>> {
+    // Since most of the message do not have url, use is_match to process them faster
+    // Trade off is that we need to process the message twice if it has url
+    if !URL_RE.is_match(&url) {
+        return None;
+    }
+
+    let mut urls: Vec<String> = Vec::new();
+    URL_RE.find_iter(&url).for_each(|matches| {
+        urls.push(matches.as_str().to_string());
+    });
+
+    Some(urls)
 }
 
 fn store_message(conn: &mut Connection, key: String, message: &Message) {
@@ -37,22 +46,53 @@ pub async fn ming_handler(ctx: Context, new_message: Message) {
     let mut is_ming = false;
     match has_url(content) {
         None => {}
-        Some(url) => {
-            debug!("URL found: {}", url);
+        Some(urls) => {
+            for u in urls {
+                debug!("URL found: {}", u);
 
-            // Try find the URL in the redis
-            // He minged if found
-            if is_exist(&mut conn, &url) {
-                debug!("URL found in redis: {}", url);
-                is_ming = true;
+                // Try find the URL in the redis
+                // He minged if found
+                if is_exist(&mut conn, &u) {
+                    debug!("URL found in redis: {}", u);
+                    is_ming = true;
+                }
+
+                // Store to redis
+                store_message(&mut conn, u, &new_message);
             }
-
-            // Store to redis
-            store_message(&mut conn, url, &new_message);
         }
     }
 
     if is_ming {
         new_message.react(&ctx.http, '💩').await.unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_has_url_0_url() {
+        let testcase = "Hello world".to_string();
+
+        let result = has_url(testcase);
+        assert!(result.is_none());
+    }
+
+    #[test]
+    fn test_has_url_1_url() {
+        let testcase = "Hello world example.com".to_string();
+
+        let result = has_url(testcase);
+        assert_eq!(result.unwrap(), vec!["example.com"]);
+    }
+
+    #[test]
+    fn test_has_url_2_url() {
+        let testcase = "Hello world example.com example2.com".to_string();
+
+        let result = has_url(testcase);
+        assert_eq!(result.unwrap(), vec!["example.com", "example2.com"]);
     }
 }
